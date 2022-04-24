cb2a561400a6228998de264f0180f8b4c839321d
Rebecca Kraft <rebeccakraft@Jakes-MacBook-Air.local>
2022-04-23 23:16:18 -0700

    finishedgit add .! Woohoo

--- FinalDemo/createUser.swift
+++ FinalDemo/createUser.swift
@@ -16,12 +16,23 @@ import CoreMedia
 import Amplify
 import AWSCognitoAuthPlugin
 
+var username:String!
+
+
 class createUser: UIViewController, StreamDelegate {
 
+@IBOutlet weak var loginUser: UITextField!
+@IBOutlet weak var loginPassword: UITextField!
 @IBOutlet weak var userNametext: UITextField!
 @IBOutlet weak var passwordtext: UITextField!
 @IBOutlet weak var emailtext: UITextField!
 @IBOutlet weak var confirmationtext: UITextField!
+@IBOutlet weak var hiddenLabel:UILabel!
+    
+var password:String!
+var email: String!
+var confirmation:String!
+    
 
 func signUp(username: String, password: String, email: String) {
     let userAttributes = [AuthUserAttribute(.email, value: email)]
@@ -39,22 +50,27 @@ func signUp(username: String, password: String, email: String) {
         }
     }
 }
-func confirmSignUp(for username: String, with confirmationCode: String) {
-    Amplify.Auth.confirmSignUp(for: username, confirmationCode: confirmationCode) { result in
-        switch result {
-        case .success:
-            print("Confirm signUp succeeded")
-        case .failure(let error):
-            print("An error occurred while confirming sign up \(error)")
+    func confirmSignUp(for username: String, with confirmationCode: String) {
+        Amplify.Auth.confirmSignUp(for: username, confirmationCode: confirmationCode) { result in
+            switch result {
+            case .success:
+               // self.hiddenLabel.text = "Your account has been confirmed!"
+                print("Confirm signUp succeeded")
+                
+            case .failure(let error):
+                print("An error occurred while confirming sign up \(error)")
+            }
         }
     }
-}
 @IBAction func createUser(_ sender: Any) {
-  //  signUp(username: userNametext.text!, password: passwordtext.text!,email: emailtext.text!)
+    username = userNametext.text!
+    password = passwordtext.text!
+    email = emailtext.text!
+    signUp(username: userNametext.text!, password: passwordtext.text!,email: emailtext.text!)
  
 }
 @IBAction func confirmCode(_ sender: Any) {
-    confirmSignUp(for: userNametext.text!, with: confirmationtext.text!)
+    confirmSignUp(for: username, with: confirmationtext.text!)
 }
 
 func fetchCurrentAuthSession() {
@@ -66,5 +82,20 @@ func fetchCurrentAuthSession() {
             print("Fetch session failed with error \(error)")
         }
     }
+}
+func signIn(username: String, password: String) {
+        Amplify.Auth.signIn(username: username, password: password) { result in
+            switch result {
+            case .success:
+                print("Sign in succeeded")
+            case .failure(let error):
+                print("Sign in failed \(error)")
+            }
+        }
+    }
+@IBAction func loginButton(_ sender: Any) {
+    
+    signIn(username: loginUser.text!, password: loginPassword.text!)
+    
 }
 }
